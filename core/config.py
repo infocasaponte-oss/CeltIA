536a8059bdd8a00b03de6627bf74c1bd69691000
infocasaponte-oss <infocasaponte@gmail.com>
2026-09-24 15:45:35 +0200

    CDE: add total request timeout setting

--- core/config.py
+++ core/config.py
@@ -38,6 +38,7 @@ class Settings(BaseSettings):
     decision_max_total_output_tokens: int = 8192
     decision_max_total_prompt_chars: int = 250000
     decision_call_timeout_seconds: float = 30.0
+    decision_request_timeout_seconds: float = 120.0
 
     # Creador Studio: proyectos aislados + sandbox Docker por proyecto
     creator_projects_dir: str = "/data/projects"
