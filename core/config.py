c9d0ef903018c0a1c020e7dfc5900f185a9d1aee
infocasaponte-oss <infocasaponte@gmail.com>
2026-09-24 15:26:03 +0200

    CDE: add total decision output budget setting

--- core/config.py
+++ core/config.py
@@ -35,6 +35,7 @@ class Settings(BaseSettings):
     decision_ood_margin_threshold: float = 0.10
     decision_max_questions: int = 32
     decision_max_output_tokens: int = 1024
+    decision_max_total_output_tokens: int = 8192
 
     # Creador Studio: proyectos aislados + sandbox Docker por proyecto
     creator_projects_dir: str = "/data/projects"
